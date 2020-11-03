0c0521fa85256d8eda08fdc7644c2c95304099ad
root <root@localhost.localdomain>
2020-11-03 14:33:24 +0100

    updated the App.js file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,5 @@
-import logo from "./logo.svg";
 import "./App.css";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Redirect
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Shape from "./containers/shape/components/shape";
 
 const App = () => {
